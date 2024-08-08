fce5e870c211780b80093e8f8834e21bb68d9c89
Titus kipchirchir <vantiger817@tgmail.com>
2024-08-08 13:57:27 +0300

    complete_lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+// Write your solution here 
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+   let newCatsArray = [...cats];
+    newCatsArray.push(name);
+    return newCatsArray
+}
+function prependCat(name) {
+    let newCatsArray = [...cats];
+    newCatsArray.unshift(name);
+    return newCatsArray;
+}
+function removeLastCat() {
+    let newCatsArray = [...cats];
+    newCatsArray.pop();
+    return newCatsArray
+}
+function removeFirstCat() {
+    let newCatsArray = [...cats];
+    newCatsArray.shift();
+    return newCatsArray
+}
